b3ad0b23313691ad5d8d2fce50145262a280ce0d
Francisco Enriquez <Francisco.R.Enriquez@hubkonnect.com>
2021-07-14 22:31:20 -0400

    Add reverse integer

---
+++ reverseInteger/main.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <sstream>
+#include <vector>
+#include <main/Main.h>
+
+int reverseInteger(int x)
+{
+    int result = 0;
+    int sign = 1;
+
+    try
+    {
+        std::string xString = std::to_string(x);
+
+        if (x < 0)
+        {
+            sign *= -1;
+            xString = xString.substr(1);
+        }
+
+        std::stringstream ss;
+
+        for (auto it = xString.rbegin(); it != xString.rend(); it++)
+        {
+            ss << *it;
+        }
+
+        result = std::stoi(ss.str());
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        result = 0;
+    }
+
+    return result * sign;
+}
+
+int main(int argc, char *argv[])
+{
+    Main lcMain(argc, argv);
+
+    auto args = lcMain.argsToInt();
+
+    std::cout << args[0] << std::endl;
+    std::cout << reverseInteger(args[0]) << std::endl;
+
+    return 0;
+}
