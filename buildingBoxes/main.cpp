51cb4351f0700302d353125d829b0efbe259b2a8
Francisco Enriquez <Francisco.R.Enriquez@hubkonnect.com>
2021-08-14 12:31:11 -0400

    Solution for building boxes

--- buildingBoxes/main.cpp
+++ buildingBoxes/main.cpp
@@ -3,39 +3,36 @@
 int minimumBoxes(const int n)
 {
     int floorCount = 0;
-    int level = 0;
     int boxesToAdd = 1;
     int boxesAdded = 0;
 
     while (boxesAdded < n)
     {
-        for (int b{boxesToAdd}; b > 0; b--)
+        for (int b{1}; b <= boxesToAdd; b++)
         {
             if ((boxesAdded + b) <= n)
             {
-                if (level++ == 0)
-                {
-                    floorCount += b;
-                }
-                boxesAdded += b;
+                floorCount++;
+                boxesAdded += 1 + (b - 1);
+            }
+            else if ((boxesAdded + 1) <= n)
+            {
+                floorCount++;
+                boxesAdded += 1 + (n - (boxesAdded + 1));
+                break;
             }
             else
             {
-                if (level == 0)
-                {
-                    floorCount += (n - boxesAdded);
-                }
-                boxesAdded += (n - boxesAdded);
                 break;
             }
         }
 
-        level = 0;
         boxesToAdd++;
     }
 
     return floorCount;
 }
+
 int main(int argc, char *argv[])
 {
     Main lcMain(argc, argv);
